9ca5d2991b48ab4deae7a419bb34e0843534d6c0
Georg Mavridis <georg.mavridis@imperia-ag.de>
2016-01-17 10:30:20 +0100

    WIP grunt excercise

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,17 @@
-        var app = angular.module('confusionApp',[]);
-        app.controller('menuController', function() {
-            this.tab = 1;
+'use strict';
+angular.module('confusionApp', [])
+
+        .controller('menuController', function() {
+                        this.tab = 1;
+            this.filtText = '';
             var dishes=[
                          {
-                           name:'Uthapizza',
-                           image: 'images/uthapizza.png',
-                           category: 'mains',
-                           label:'Hot',
-                           price:'4.99',
-                           description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains',
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
                            comment: ''
                         },
                         {
@@ -17,7 +20,7 @@
                            category: 'appetizer',
                            label:'',
                            price:'1.99',
-                           description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+                          description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
                            comment: ''
                         },
                         {
@@ -26,36 +29,37 @@
                            category: 'appetizer',
                            label:'New',
                            price:'1.99',
-                           description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+                          description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
                            comment: ''
                         },
                         {
                            name:'ElaiCheese Cake',
                            image: 'images/elaicheesecake.png',
-                           category: 'dessert',
+                          category: 'dessert',
                            label:'',
-                           price:'2.99',
-                           description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+                          price:'2.99',
+                          description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
                            comment: ''
                         }
-                        ];
+                        ]; 
+             this.dishes = dishes;
 
-            this.dishes = dishes;
-            this.filtText = '';
-            this.select = function(setTab) {
+             this.select = function(setTab) {
                 this.tab = setTab;
-                
-                if (setTab === 2)
+                if (setTab === 2) {
                     this.filtText = "appetizer";
-                else if (setTab === 3)
+                }
+                else if (setTab === 3) {
                     this.filtText = "mains";
-                else if (setTab === 4)
+                }
+                else if (setTab === 4) {
                     this.filtText = "dessert";
-                else
+                }
+                else {
                     this.filtText = "";
-            }
+                }
+            };
             this.isSelected = function (checkTab) {
                 return (this.tab === checkTab);
-            }
+            };
         });
-
