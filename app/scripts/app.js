7ac04be440f02606d4c9de096ed04b973094ce3c
Georg Mavridis <georg.mavridis@imperia-ag.de>
2016-01-24 12:19:46 +0100

    ass2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -90,7 +90,7 @@ angular.module('confusionApp', [])
                 
                 console.log($scope.feedback);
                 
-                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
                 }
@@ -155,24 +155,32 @@ angular.module('confusionApp', [])
         .controller('DishCommentController', ['$scope', function($scope) {
             
             //Step 1: Create a JavaScript object to hold the comment from the form
-            $scope.feedback = {
+            $scope.comment = {
                                    rating: 5,
-                                   comment:"",
-                                   author:""
+                                   comment: "",
+                                   author: "",
+                                   date: new Date().toISOString()
                                };
 
             $scope.submitComment = function () {
                 
                 //Step 2: This is how you record the date
                 //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
-                
+                $scope.comment.date = new Date().toISOString();
                 // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+                $scope.dish.comments.push($scope.comment);
                 
                 //Step 4: reset your form to pristine
+                $scope.commentForm.$setPristine();
                 
                 //Step 5: reset your JavaScript object that holds your comment
-            }
+                $scope.comment = {
+                                   rating: 5,
+                                   comment: "",
+                                   author: "",
+                                   date: new Date().toISOString()
+                               };
+            };
         }])
 
 ;
