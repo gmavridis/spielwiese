f96fa3425ceebc2d3d9edc8880bf3c59264aec8c
Georg Mavridis <georg.mavridis@imperia-ag.de>
2016-01-30 21:59:11 +0100

    Starting 3rd Assignment

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,6 +9,7 @@ angular.module('confusionApp')
             $scope.showDetails = false;
 
             $scope.dishes= menuFactory.getDishes();
+
                         
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -53,7 +54,7 @@ angular.module('confusionApp')
                 
                 console.log($scope.feedback);
                 
-                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
                 }
@@ -68,39 +69,31 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+
             var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-                        $scope.dish = dish;
-                    }])
+            
+            $scope.dish = dish;
+            
+        }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
-           
-            //Step 1: Create a JavaScript object to hold the comment from the form
-            $scope.comment = {
-                                   rating: 5,
-                                   comment: "",
-                                   author: "",
-                                   date: new Date().toISOString()
-                               };
-
+            
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            
             $scope.submitComment = function () {
                 
-                //Step 2: This is how you record the date
-                //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
-                $scope.comment.date = new Date().toISOString();
-                // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push($scope.comment);
+                $scope.mycomment.date = new Date().toISOString();
+                console.log($scope.mycomment);
                 
-                //Step 4: reset your form to pristine
+                $scope.dish.comments.push($scope.mycomment);
+                
                 $scope.commentForm.$setPristine();
                 
-                //Step 5: reset your JavaScript object that holds your comment
-                $scope.comment = {
-                                   rating: 5,
-                                   comment: "",
-                                   author: "",
-                                   date: new Date().toISOString()
-                               };
-            };
+                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            }
         }])
+
+        // implement the IndexController and About Controller here
+
 
 ;
