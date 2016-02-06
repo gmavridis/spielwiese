cc4c20250c1e4f824b8424551fadda1511d7972c
Georg Mavridis <georg.mavridis@imperia-ag.de>
2016-02-06 13:55:05 +0100

    ngResource Exercise

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,19 +8,9 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.showMenu = false;
+            $scope.showMenu = true;
             $scope.message = "Loading ...";
-            $scope.dishes= {};
-            menuFactory.getDishes().then(
-                function(response) {
-                    $scope.dishes = response.data;
-                    $scope.showMenu = true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );
-                        
+            $scope.dishes = menuFactory.getDishes().query();                        
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
                 
@@ -79,18 +69,9 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-            $scope.dish = {};
-            $scope.showDish = false;
+            $scope.showDish = true;
             $scope.message="Loading ...";
-            menuFactory.getDish(parseInt($stateParams.id,10)).then(
-                function(response){
-                    $scope.dish = response.data;
-                    $scope.showDish=true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
@@ -112,19 +93,9 @@ angular.module('confusionApp')
 
         // implement the IndexController and About Controller here
         .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory) {
-            $scope.dish = {};
-            $scope.showDish = false;
+            $scope.showDish = true;
             $scope.message="Loading ...";
-
-            menuFactory.getDish(0).then(
-                function(response){
-                    $scope.dish = response.data;
-                    $scope.showDish = true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );
+            $scope.dish = menuFactory.getDishes().get({id:0});
             $scope.promotion = menuFactory.getPromotion(0);
             $scope.leader = corporateFactory.getLeader(3);
             
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -3,7 +3,7 @@
 angular.module('confusionApp')
 
         .constant('baseURL', 'http://localhost:3000/')
-        .service('menuFactory', ['$http', 'baseURL', function($http,baseURL) {
+        .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
             var promotions = [
                 {
                           _id:0,
@@ -16,15 +16,10 @@ angular.module('confusionApp')
                 
             ];
     
-            this.getDishes = function(){
-                                return $http.get(baseURL+"dishes");
-            };
-            this.getDish = function (index) {
-                                return $http.get(baseURL+"dishes/"+index);
-            };
-            // implement a function named getPromotion
-            // that returns a selected promotion.
 
+            this.getDishes = function(){
+                return $resource(baseURL+"dishes/:id",null,  {'update':{method:'PUT' }});
+            };
             this.getPromotion = function (index) {
                
                 return promotions[index];
